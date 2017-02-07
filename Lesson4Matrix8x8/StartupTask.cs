4ea04a0e57a305974fe24d5c003fb2fe7d70958e
Leivo Sepp <leivo@robootika.com>
2017-02-07 12:26:50 +0200

    Add project files.

---
+++ Lesson4Matrix8x8/StartupTask.cs
@@ -0,0 +1,22 @@
+﻿using Windows.ApplicationModel.Background;
+using Glovebox.Graphics.Drivers;
+using Glovebox.Graphics.Components;
+
+namespace Lesson4Matrix8x8
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        Ht16K33 driver = new Ht16K33(new byte[] { 0x70 }, Ht16K33.Rotate.None);
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            LED8x8Matrix matrix = new LED8x8Matrix(driver);
+
+            while (true)
+            {
+                var message = $"Tere, Robootika.COM";
+                matrix.ScrollStringInFromRight(message, 70);
+            }
+        }
+    }
+}
